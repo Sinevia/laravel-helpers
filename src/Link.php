d01d25e90de0f8d7b579756b84e5c38693a7a5db
Sinevia <info@sinevia.com>
2018-08-15 06:57:36 +0100

    Update Link.php

--- src/Link.php
+++ src/Link.php
@@ -4,7 +4,7 @@ namespace Sinevia\LaravelHelpers;
 
 class Link {
 
-    prrotected static function query($queryData = []) {
+    protected static function query($queryData = []) {
         $queryString = '';
         if (count($queryData)) {
             $queryString = '?' . http_build_query($queryData);
